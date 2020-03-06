7e44680a4f6c7f5602269ec692ab09c273ce6e6d
Sophia Sun <sophiaphia@live.com>
2020-03-06 00:15:58 -0800

    uh

--- lyrics/preprocess.py
+++ lyrics/preprocess.py
@@ -5,7 +5,7 @@ files = os.listdir("./lyrics/samples")
 
 with open('./vis/AttnGAN/data/coco/example_filenames.txt', 'w') as f:
 	for file in files:
-		if f[-3:] == "txt":
+		if file[-3:] == "txt":
 			f.write('../../../lyrics/samples/{}\n'.format(file))
 
 
