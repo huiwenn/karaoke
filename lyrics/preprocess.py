e25e65a068f0ec20f55eccdbca02d2cc7f016e96
Sophia Sun <sophiaphia@live.com>
2020-03-07 00:08:10 -0800

    another try

--- lyrics/preprocess.py
+++ lyrics/preprocess.py
@@ -9,14 +9,14 @@ for f in files:
 
 	song_name = f[:-4]
 
-	os.mkdir("./lyrics/{}".format(song_name))
+	os.mkdir("./lyrics/{}".format(song_name), exist_ok=True)
 	ct = 0
 
 	with open("./lyrics/samples/{}".format(f), 'r') as lyr:
 		lines = lyr.readlines()
 	
 	for i in range(int(len(lines)/10)):
-		pth = "./lyrics/tmp/{}_{}".format((song_name, i))
+		pth = "./lyrics/tmp/{}_{}".format(song_name, i)
 		print(pth)
 		with open(pth, 'w') as writefile:
 			if 10*(i+1) >= len(lines):
