4883e5c2e661bdacaad24432c737a606c0933a8b
Sophia Sun <sophiaphia@live.com>
2020-03-06 16:01:23 -0800

    script

--- lyrics/preprocess.py
+++ lyrics/preprocess.py
@@ -2,9 +2,32 @@ import os
 
 files = os.listdir("./lyrics/samples")
 
+for f in files:
+	if file[-3:] != "txt":
+		continue
 
-with open('./vis/AttnGAN/data/coco/example_filenames.txt', 'w') as f:
-	for file in files:
-		if file[-3:] == "txt":
-			f.write('../../../lyrics/samples/{}\n'.format(file[:-4]))
+	song_name = file[:-4]
+
+	# first: make small batches
+	os.mkdir("./lyrics/{}".format(song_name))
+	ct = 0
+
+	with open("./lyrics/samples/{}".format(f), 'r'):
+		lines = f.readlines()
+	
+	fnames = []
+
+	for i in range(len(lines)/10):
+		pth = "./lyrics/tmp/{}_{}".format((song_name, i))
+		with open(pth, 'w') as writefile:
+			writefile.write(''.join(lines[i*10:(i+1)*10]))
+
+		with open('./vis/AttnGAN/data/coco/example_filenames.txt', 'w') as f:
+			f.write('../../.{}}'.format(pth))
+
+		os.chdir('vis/AttnGAN/code')
+		os.system("python2 main.py --cfg cfg/eval_coco.yml --gpu 0")
+		os.chdir("../../..")
+
+
 
